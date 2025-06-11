a6bb56c0baab268b0f7ae6330d3d96ce658b5c29
MegaValentin <91918490+MegaValentin@users.noreply.github.com>
2025-06-11 13:32:17 -0300

    validator mongodb

--- src/middleware/validator.mongoId.js
+++ src/middleware/validator.mongoId.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
-export const validateMongoId = (req, res, next)  => {
-    const { id } = req.params
-
-    if(!mongoose.Type.ObjectId.isValid(id)){
-        return res.status(400).json({ message: "ID invalido"})
+export function validateMongoId(req, res, next) {
+    const { id } = req.params;
+  
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "ID no válido de MongoDB" });
     }
-
-    next()
-}
+  
+    next();
+  }
